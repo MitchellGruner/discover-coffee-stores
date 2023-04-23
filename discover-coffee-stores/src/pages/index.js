b3cb071e225250e647f9e4f848ef4aef78141178
Mitchell Gruner <mit.gruner@gmail.com>
2023-04-23 13:39:49 -0500

    add hero image

--- discover-coffee-stores/src/pages/index.js
+++ discover-coffee-stores/src/pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Banner from "../../components/banner";
+import Card from "../../components/card";
 
 export default function Home() {
 
@@ -17,6 +19,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <Banner buttonText={"View Stores Nearby"} handleOnClick={handleOnBannerBtnClick} />
+        <div className={styles.heroImage}>
+          <img src="/static/hero-image.png" width={700} height={400} />
+        </div>
+        <Card name="DarkHorse Coffee" imgUrl="/static/hero-image.png" href="/coffee-store/darkhorse-coffee" />
       </main>
     </div>
   )

a5b6e5704c537dc3cc9db6ed63100858c91ff05e
Mitchell Gruner <mit.gruner@gmail.com>
2023-04-23 23:11:53 -0500

    make page's content unique based on id

--- discover-coffee-stores/src/pages/coffee-store/[id].js
+++ discover-coffee-stores/src/pages/coffee-store/[id].js
@@ -1,15 +1,42 @@
 import {useRouter} from "next/router";
 import Link from "next/link";
 
-const CoffeeStore = () => {
+import coffeeStoresData from "../../../data/coffee-stores.json";
+
+export function getStaticProps(staticProps) {
+    const params = staticProps.params;
+
+    return {
+        props: {
+            coffeeStore: coffeeStoresData.find(coffeeStore => {
+                return coffeeStore.id.toString() === params.id;
+            }),
+        },
+    };
+}
+
+export function getStaticPaths() {
+    return {
+        paths: [
+            { params: { id: '0' } },
+            { params: { id: '1' } },
+            { params: { id: '300'} }
+        ],
+        fallback: true
+    }
+}
+
+const CoffeeStore = (props) => {
     const router = useRouter();
-    console.log("router", router);
+
     return (
         <div>
             Coffee Store Page {router.query.id}
             <Link href="/">
                 Back to Home
             </Link>
+            <p>{props.coffeeStore.address}</p>
+            <p>{props.coffeeStore.name}</p>
         </div>
     );
 };
